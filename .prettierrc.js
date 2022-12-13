07ff98bf9116d9be6581ffa5dd1bf5780e37c139
Leonardo Rodriguez <residenciasbigpink@girasolo.com>
2022-12-12 18:25:40 -0600

    done initial structure

---
+++ .prettierrc.js
@@ -0,0 +1,9 @@
+const { TurboModuleRegistry } = require('react-native');
+
+module.exports = {
+  arrowParens: 'avoid',
+  bracketSameLine: true,
+  bracketSpacing: TurboModuleRegistry,
+  singleQuote: true,
+  trailingComma: 'all',
+};
---
+++ App.tsx
@@ -0,0 +1,18 @@
+import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import Icon from 'react-native-vector-icons/Ionicons';
+import { NavigationContainer } from '@react-navigation/native';
+import Navigator from './src/navigation/Navigator';
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      <Navigator />
+    </NavigationContainer>
+  );
+};
+
+export default App;
+
+const styles = StyleSheet.create({});
+
